34b9dd3155005f7457c69a33d0dff38b2a231c55
Julean Hommel <juleanhommel@gmail.com>
2021-10-07 19:43:17 +0200

    Players can join a Game

--- Gotcha 2.0/Controllers/PlayerController.cs
+++ Gotcha 2.0/Controllers/PlayerController.cs
@@ -1,25 +1,46 @@
-﻿using Gotcha_2._0.DataAccess;
-using Gotcha_2._0.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Gotcha_2._0.Models;
+using Gotcha_2._0.DataAccess;
 
 namespace Gotcha_2._0.Controllers
 {
     public class PlayerController : Controller
     {
+        GameData GameDB = new GameData();
+        List<Game> games = new List<Game>();
+        Game game = new Game();
+
+        ContractData ContractDB = new ContractData();
+        List<Contract> contracts = new List<Contract>();
+        Contract contract = new Contract();
+
         // GET: Player
         public ActionResult Index()
         {
+            Session["CurentUser_Id"] = "1";
             return View();
         }
-
-        List<Game> games = new List<Game>();
+        
         public ActionResult GameList()
         {
+            games = GameDB.GetGames();
+
             return View(games);
+        }
+
+        public ActionResult GameJoin(int id)
+        {
+            contract.Game_Id = id;
+            contract.User_Id = Convert.ToInt32(Session["CurentUser_Id"]);
+            contract.EliminationTime = DateTime.Now;
+
+            ContractDB.AddContract(contract);
+
+            return RedirectToAction("GameList", "Player");
         }
     }
 }
--- Gotcha 2.0/DataAccess/ContractData.cs
+++ Gotcha 2.0/DataAccess/ContractData.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Gotcha_2._0.Models;
+using Dapper;
+using System.Data;
 
 namespace Gotcha_2._0.DataAccess
 {
     public class ContractData
     {
+        public void AddContract(Contract contract)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("GotchaDB")))
+            {
+                string insertQuery = @"INSERT INTO [dbo].[Contract](EliminationTime, Eliminations, Word_Id, User_Id, Game_Id) 
+                                       VALUES (@EliminationTime, @Eliminations, @Word_Id, @User_Id, @Game_Id)";
+
+                var result = connection.Execute(insertQuery, contract);
+            }
+        }
     }
 }
